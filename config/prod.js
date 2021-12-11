196a65174b247acad5813dd478c29596b35fad27
ripanionut <ripan_ionut@yahoo.com>
2021-12-12 01:49:29 +0200

    plss

--- config/prod.js
+++ config/prod.js
@@ -2,14 +2,11 @@
 
     
   module.exports = {
-    DB_URI_AFT:process.env.DB_URI_AFT,
-    AUTH0_SECRET=process.env.AUTH0_SECRET,
-    AUTH0_BASE_URL=process.env.AUTH0_BASE_URL,
-    AUTH0_ISSUER_BASE_URL=process.env.AUTH0_ISSUER_BASE_URL,
-    AUTH0_CLIENT_ID=process.env.AUTH0_CLIENT_ID,
-    AUTH0_CLIENT_SECRET=process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_AUDIENCE=process.env. AUTH0_AUDIENCE,
-    AUTH0_SCOPE=process.env.UTH0_SCOPE,
-    PORTFOLIO_API_URL=process.env.PORTFOLIO_API_URL,
-    AUTH0_NAMESPACE=process.env.AUTH0_NAMESPACE,
+    DB_URI_AFT: process.env.DB_URI_AFT,
+    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
+    DB_URI_AFT:
+      "mongodb+srv://ripanionut2:ripanionut@cluster1.aeufk.mongodb.net/portfolioAFT?retryWrites=true&w=majority",
   };
